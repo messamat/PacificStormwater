f933a625e380d43d54229556f22164ae91d9a0e2
Mathis Loïc Messager <messamat@uw.edu>
2018-09-27 10:56:25 -0700

    Merge and rasterize all datasets

--- CollateLU.py
+++ CollateLU.py
@@ -8,52 +8,76 @@ import re
 arcpy.env.overwriteOutput = True
 arcpy.env.qualifiedFieldNames = False
 
-
 #Input data
 rootdir = 'C:\Mathis\ICSL\stormwater'
 datadir = os.path.join(rootdir,'data')
 resdir = os.path.join(rootdir,'results')
+NLCD = os.path.join(datadir, 'nlcd_2011_landcover_2011_edition_2014_10_10\\nlcd_2011_landcover_2011_edition_2014_10_10.img')
 CAdir = os.path.join(datadir,'CA_LULC')
 ORzoning = os.path.join(datadir,'Oregon_Zoning_2017/Oregon_Zoning_2017.shp')
-LAzoning = os.path.join(CAdir, 'LosAngeles/General_Plan_Land_Use__Los_Angeles/Los_Angeles_PL_SCAG.shp')
+LAzoning = os.path.join(CAdir, 'LosAngeles/General_Plan_Land_Use__Los_Angeles/LA.gdb/Los_Angeles_PL_SCAG.shp')
 SanDiegozoning = os.path.join(CAdir, 'SanDiego\LANDUSE_CURRENT\LANDUSE_CURRENT.shp')
 SanBerzoning = os.path.join(CAdir, 'SanBernardino\SBCo_Parcel_Polygons\SBCo_Parcel_Polygons.shp')
 ContraCostazoning = os.path.join(CAdir, 'ContraCosta/PLA_DCD_Zoning/PLA_DCD_Zoning.shp')
 Kernzoning = os.path.join(CAdir, 'Kern/Zoning/Zoning.shp')
 Montereyzoning = os.path.join(CAdir, 'Monterey/Zoning/Zoning.shp')
 SantaCruzzoning = os.path.join(CAdir, 'SantaCruz/Zoning/Zoning.shp')
-SCAGzoning = os.path.join(CAdir, 'SCAG/landuse_poly_SCAG_2012.shp')
+SCAGzoning = os.path.join(CAdir, 'SCAG/SCAG.gdb/landuse_poly_SCAG_2012.shp')
+LUint_reclass = os.path.join(CAdir, 'LUintensity_reclass.csv')
+
+WAzoning = 'C:/Mathis/ICSL/flood_vulnerability/results/flood_risk.gdb/parcel_o5cleanall_dissolve' #Completely different directory WARNING
 
 arcpy.env.workspace = resdir
 
 #Output variables
+WAreclass = os.path.join(resdir, 'WAzoning_reclass.shp')
+ORreclass = ORzoning[:-4]+'_reclass.shp'
 CDWR_merge = os.path.join(resdir, 'CDWR_merge.shp')
 CDWR_merge_noZ = os.path.join(resdir, 'CDWR_merge_noZ.shp')
-LAreclass = LAzoning[:-4]+'_reclass.shp'
+LAreclass = LAzoning[:-4]+'_reclass'
 SanDiegoreclass = SanDiegozoning[:-4]+'_reclass.shp'
 SanBerreclass = SanBerzoning[:-4]+'_reclass.shp'
 ContraCostareclass = ContraCostazoning[:-4]+'_reclass.shp'
 Kernreclass = Kernzoning[:-4]+'_reclass.shp'
 Montereyreclass = Montereyzoning[:-4]+'_reclass.shp'
 SantaCruzreclass = SantaCruzzoning[:-4]+'_reclass.shp'
-SCAGreclass = SCAGzoning[:-4]+'_reclass.shp'
+SCAGreclass = SCAGzoning[:-4]+'_reclass'
+
+LU_gdb = os.path.join(resdir, 'LU.gdb')
+if not arcpy.Exists(LU_gdb):
+    arcpy.CreateFileGDB_management(resdir, 'LU.gdb')
+CALUall = os.path.join(LU_gdb, 'CALU_all')
+CALUurb = os.path.join(LU_gdb,'CALU_urban')
+CALUras = os.path.join(LU_gdb,'CALU_urbanras')
+WALUras = os.path.join(LU_gdb, 'WALU_urbanras')
+ORLUras = os.path.join(LU_gdb, 'ORLU_urbanras')
 
 #Define functions
 def listunique(feature, field) :
     '''Function to write out a csv table listing unique values of a field'''
     ulist = [field]
     [ulist.append(row[0]) for row in arcpy.da.SearchCursor(feature, [field]) if row[0] not in ulist]
-    with open(os.path.join(resdir,feature+'reclass.csv'), 'wb') as myfile:
+    with open(os.path.join(resdir,os.path.basename(feature)+'reclass.csv'), 'wb') as myfile:
         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
         wr.writerow(ulist)
 
+crs= arcpy.Describe(NLCD).SpatialReference
+
 # ----------------------------------- FORMAT INDIVIDUAL DATASETS -------------------------------------------------------
+#Reclassify Washington parcel dataset
+listunique(WAzoning, 'StateLandUseCD') #Then manually format and classify into Agriculture, Residential, Commercial, Industrial, Miscellaneous in Excel
+arcpy.MakeFeatureLayer_management(WAzoning, 'WAzoning_lyr')
+arcpy.AddJoin_management('WAzoning_lyr', 'StateLandUseCD',os.path.join(resdir,'WAzoningreclass_edit.csv'),
+                         'StateLandUseCD', 'KEEP_COMMON')
+arcpy.CopyFeatures_management('WAzoning_lyr', WAreclass)
 
 #Reclassify Oregon zoning dataset
 listunique(ORzoning, 'orZDesc') #Then manually format and classify into Agriculture, Residential, Commercial, Industrial, Miscellaneous in Excel
 arcpy.MakeFeatureLayer_management(ORzoning, 'ORzoning_lyr')
-arcpy.AddJoin_management('ORzoning_lyr', 'orZDesc',
-                         os.path.join(datadir,'Oregon_Zoning_2017/ORzoningreclass_edit.csv'), 'orZDesc')
+arcpy.AddJoin_management('ORzoning_lyr', 'orZDesc',os.path.join(datadir,'Oregon_Zoning_2017/ORzoningreclass_edit.csv'),
+                         'orZDesc', 'KEEP_COMMON')
+arcpy.CopyFeatures_management('ORzoning_lyr', ORreclass)
+
 
 #Merge all California Department of Water Resources Land Use datasets
 regx_dir = '.*\d.*'
@@ -68,15 +92,18 @@ arcpy.Merge_management(inputs=CAdatlist, output=CDWR_merge)
 arcpy.MakeFeatureLayer_management(CDWR_merge, 'CDWR_mergelyr', where_clause="NOT CLASS1 = 'Z'")
 #Get unique zoning values
 listunique(CDWR_merge_noZ, 'CLASS1')
-arcpy.AddJoin_management('CDWR_mergelyr','CLASS1',os.path.join(CAdir,'CDWR_reclass_edit.csv'),'CLASS1')
+arcpy.AddJoin_management('CDWR_mergelyr','CLASS1',os.path.join(CAdir,'CDWR_reclass_edit.csv'),'CLASS1', 'KEEP_COMMON')
 arcpy.CopyFeatures_management('CDWR_mergelyr', CDWR_merge_noZ)
+#arcpy.MakeFeatureLayer_management(CDWR_merge_noZ, 'CDWR_mergelyr', where_clause="FID = 228570")
+#arcpy.DeleteFeatures_management('CDWR_mergelyr')
 
 #Los Angeles
 listunique(LAzoning, 'SCAG_GP_CO')
 arcpy.MakeFeatureLayer_management(LAzoning, 'LAzoninglyr')
 arcpy.AddJoin_management('LAzoninglyr','SCAG_GP_CO',
-                         os.path.join(CAdir,'LosAngeles/General_Plan_Land_Use__Los_Angeles/Los_Angeles_PL_SCAGreclass_edit.csv'),'SCAG_GP_CO')
-arcpy.CopyFeatures_management('LAzoninglyr', LAreclass)
+                         os.path.join(CAdir,'LosAngeles/General_Plan_Land_Use__Los_Angeles/Los_Angeles_PL_SCAGreclass_edit.csv'),
+                         'SCAG_GP_CO', 'KEEP_COMMON')
+arcpy.CopyFeatures_management('LAzoninglyr', LAreclass) #Can't do that in Python unless in same gdb
 arcpy.AddField_management(LAreclass, 'SURVEYYEAR','LONG',10)
 arcpy.CalculateField_management(LAreclass, 'SURVEYYEAR', 2012, expression_type='PYTHON')
 
@@ -130,11 +157,76 @@ arcpy.AddField_management(SantaCruzreclass, 'SURVEYYEAR','LONG',10)
 arcpy.CalculateField_management(SantaCruzreclass, 'SURVEYYEAR', 2017, expression_type='PYTHON')
 
 #SCAG
-#Add 'SURVEYYEAR'
+listunique(SCAGzoning, 'LU12')
+arcpy.MakeFeatureLayer_management(SCAGzoning, 'SCAGzoninglyr')
+arcpy.AddJoin_management('SCAGzoninglyr','LU12', SCAGzoning[:-4]+'reclass_edit.csv','LU12','KEEP_COMMON')  #Can't do that in Python unless in same gdb
+arcpy.CopyFeatures_management('SCAGzoninglyr', SCAGreclass)
+arcpy.AddField_management(SCAGreclass, 'SURVEYYEAR','LONG',10) #Corrupts the shapefile (?), do it in Arcmap
+arcpy.CalculateField_management(SCAGreclass, 'SURVEYYEAR', 2012, expression_type='PYTHON')
 
-# ----------------------------------- MERGE DATASETS -------------------------------------------------------
-
-
-
-#Still missing: Tehama (missing in database, should be a 2012 layer),
+#Still missing: Tehama (only 1999 data for now, update with 2012 layer when get it),
 #Normal to be missing (don't drain to west coast): Nevada, Sierra, Mono, Inyo, Alpine
+
+# ----------------------------------- MERGE CALIFORNIA DATASETS -------------------------------------------------------
+#Merge all polygons
+mergedatlist = [CDWR_merge_noZ,LAreclass,SanDiegoreclass,SanBerreclass,ContraCostareclass,Kernreclass,
+                        Montereyreclass,SantaCruzreclass, SCAGreclass]
+mergeready = True
+for fc in mergedatlist:
+    if not arcpy.Exists(fc):
+        print('{} does not exist'.format(fc))
+        mergeready = False
+
+mergedatlistproj = []
+for i in range(0,len(mergedatlist)):
+    path = os.path.splitext(mergedatlist[i])
+    outpath = path[0] + '_proj' + path[1]
+    if arcpy.Describe(mergedatlist[i]).SpatialReference.name != crs.name and \
+         not (arcpy.Exists(outpath)):
+            print('Project '+ mergedatlist[i])
+            arcpy.Project_management(mergedatlist[i], outpath, crs)
+            mergedatlistproj.append(outpath)
+    else:
+        mergedatlistproj.append(mergedatlist[i])
+
+#Errors during merge:
+#ExecuteError: ERROR 001156: Failed on input OID 288, could not write value 'The "HR" regulations apply to the following structures in the Tassajara Hot Springs complex: dining room, stonerooms and office. (See historic resources file.)' to output field Notes
+# for fc in mergedatlist:
+#     if arcpy.Exists(fc):
+#         for f in arcpy.ListFields(fc):
+#             if f.name=='Notes':
+#                 print(fc)
+arcpy.DeleteField_management(Kernreclass, 'Notes')
+if mergeready == True:
+    arcpy.Merge_management(mergedatlistproj, CALUall)
+#Only keep urban pixels
+#Create table that associates StormClass: residential:1, commercial:2, industrial:3
+arcpy.MakeFeatureLayer_management(CALUall, 'CALU_alllyr')
+arcpy.AddJoin_management('CALU_alllyr', 'StormClass', LUint_reclass, 'StormClass', 'KEEP_COMMON')
+arcpy.CopyFeatures_management('CALU_alllyr', CALUurb)
+########################## TO TEST/RUN ############################
+#Delete full merged dataset
+arcpy.Delete_management('CALU_alllyr')
+arcpy.Delete_management(CALUall)
+
+#Rasterize to same extent and resolution as NLCD keeping maximum LU intensity when overlap
+arcpy.env.extent = NLCD
+arcpy.PolygonToRaster_conversion(CALUurb, 'Luintensity', CALUras, cell_assignment='MAXIMUM_AREA',
+                                 priority_field='Luintensity', cellsize=NLCD)
+#(assume that most recent LU is always more intense)
+
+#Rasterize Washington and Oregon datasets as well
+arcpy.MakeFeatureLayer_management(WAreclass, 'WAreclasslyr')
+arcpy.AddJoin_management('WAreclasslyr', 'StormClass', LUint_reclass, 'StormClass', 'KEEP_COMMON')
+arcpy.PolygonToRaster_conversion('WAreclasslyr', os.path.split(LUint_reclass)[1]+'.Luintensity', WALUras, cell_assignment='MAXIMUM_AREA',
+                                 priority_field=os.path.split(LUint_reclass)[1]+'.Luintensity', cellsize=NLCD)
+
+arcpy.MakeFeatureLayer_management(ORreclass, 'ORreclasslyr')
+arcpy.AddJoin_management('ORreclasslyr', 'StormClass', LUint_reclass, 'StormClass', 'KEEP_COMMON')
+arcpy.PolygonToRaster_conversion('ORreclasslyr', os.path.split(LUint_reclass)[1]+'.Luintensity', ORLUras, cell_assignment='MAXIMUM_AREA',
+                                 priority_field=os.path.split(LUint_reclass)[1]+'.Luintensity', cellsize=NLCD)
+
+#Merge all rasters
+#Create residential, commercial, industrial dataset
+
+
